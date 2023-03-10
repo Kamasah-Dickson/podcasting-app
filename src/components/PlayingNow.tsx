3cc41ad3216f7f7aff1a4ae99388071b4cc2288f
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-10 00:33:17 +0000

    update playing now

--- src/components/PlayingNow.tsx
+++ src/components/PlayingNow.tsx
@@ -6,13 +6,13 @@ import Image from "next/image";
 
 function PlayingNow() {
 	return (
-		<div className="max-w-[450px] md:sticky md:top-0 md:right-0 md:h-screen md:w-full">
-			<div className=" w-full rounded-lg bg-[#2d0796] p-4 md:w-[290px] ">
-				<div className="h-[230px] w-full rounded-lg">
+		<div className="pt-3 md:sticky md:top-0 md:right-0 md:h-screen md:w-full">
+			<div className="rounded-lg bg-[#2d0796] p-3 md:p-5">
+				<div className="h-[240px] w-full rounded-2xl p-1 md:p-0">
 					<Image
 						src={me}
 						alt="sdfsd"
-						className=" h-full w-full rounded-xl object-cover"
+						className=" h-full w-full rounded-2xl object-cover"
 					/>
 				</div>
 				<div className="p-2 text-center text-white">
