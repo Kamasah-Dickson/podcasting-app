221df06555445a2670f29fe751777d9aabc124a2
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-11 05:25:00 +0000

    update sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,32 @@
 import { sideLinks } from "@/sidebarData";
 import Link from "next/link";
-import React from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 function Sidebar() {
 	const { pathname } = useRouter();
+	const [location, setLocation] = useState("");
+
+	useEffect(() => {
+		try {
+			const getCountry = async () => {
+				const response = await fetch("https://ipapi.co/json/");
+				const data = await response.json();
+				setLocation(data.country_name);
+			};
+			getCountry();
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
 
 	return (
 		<div className="fixed bottom-0 z-10  w-full bg-[#16151b] sm:sticky sm:top-0 sm:left-0 sm:h-screen">
-			<Link href="/" className=" relative block hidden text-center md:block">
+			<Link href="/" className=" relative hidden text-center md:block">
 				<h1 className="relative mx-auto w-fit py-3 text-4xl font-medium capitalize text-white">
 					<span className="text-purple-500">Po</span>dcast.
 					<div className="absolute top-0 -right-3 text-[11px] text-gray-500">
-						GH
+						{location}
 					</div>
 				</h1>
 			</Link>
@@ -25,7 +39,7 @@ function Sidebar() {
 									pathname === data.path && "my-hover"
 								} hover:my-hover flex items-center gap-3 py-2 px-4`}
 								key={data.id}
-								href={`/${data.path}`}
+								href={`${data.path}`}
 							>
 								<p className="text-xl">{data.icon}</p>
 								<p className="hidden md:flex">{data.name}</p>
