eccb8c5e84acd295ca5d3f12c8aee7b6ba5575f2
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-12 04:37:16 +0000

    configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["production.listennotes.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
