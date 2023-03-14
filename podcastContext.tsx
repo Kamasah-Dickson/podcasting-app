b6447bc608a2dd1ef8e765924f6009defd7c25f9
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-14 22:07:09 +0000

    update context

--- podcastContext.tsx
+++ podcastContext.tsx
@@ -6,7 +6,8 @@ interface podcast {
 	id: string;
 	title: string;
 	audio: string;
-	image: string;
+	author: string;
+	big_cover_url: string;
 }
 
 interface podcastType {
@@ -29,28 +30,30 @@ export function PodcastProvider({ children }: Props) {
 	const [podcast, setPodcast] = useState<podcast[]>([]);
 	const [error, setError] = useState<string>("");
 	const [loading, setLoading] = useState<boolean>(true);
-	const [countrycode, setCountrycode] = useState("");
 
 	useEffect(() => {
 		setLoading(false);
 		const fetchPodcasts = async () => {
 			try {
-				const response1 = await fetch("https://ipapi.co/json/");
-				const { country_code } = await response1.json();
-				setCountrycode(country_code);
-				const response2 = await axios.get(
-					`https://listen-api.listennotes.com/api/v2/best_podcasts?page=1&country=gh&sort=listen_score&safe_mode=0`,
+				const response = await fetch("https://ipapi.co/json/");
+				const { country_code } = await response.json();
+				console.log(country_code);
 
-					{
-						headers: {
-							"X-ListenAPI-Key": "732e9fdfa7b24ea99aec390d986df8be",
-						},
-					}
-				);
-				setPodcast(response2.data.podcasts);
-				setError("");
+				const options = {
+					method: "GET",
+					url: "https://podcast-api1.p.rapidapi.com/top_channels/v2",
+					params: { category_id: "10030", country: country_code },
+					headers: {
+						"X-RapidAPI-Key":
+							"3463f3b065msh692b26db6cdd5e0p1f756ajsnf4e5cc452936",
+						"X-RapidAPI-Host": "podcast-api1.p.rapidapi.com",
+					},
+				};
+
+				const response2 = await axios.request(options);
+				setPodcast(response2.data.data.list.slice(0, 25));
 			} catch (error: any) {
-				console.log(error);
+				console.error(error);
 				setError(error.message);
 			}
 		};
