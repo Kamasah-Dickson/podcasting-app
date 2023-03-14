d6d721e81d1640642a8ea590de2a73102be5c2b0
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-14 22:06:22 +0000

    update next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["production.listennotes.com"],
+		domains: [
+			"is4-ssl.mzstatic.com",
+			"is2-ssl.mzstatic.com",
+			"ssl-static.libsyn.com",
+			"is3-ssl.mzstatic.com",
+			"is5-ssl.mzstatic.com",
+			"is1-ssl.mzstatic.com",
+			"images.transistor.fm",
+			"assets.pippa.io",
+		],
 	},
 };
 
