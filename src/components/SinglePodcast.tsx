eea614f751acfb0cbda2af31446a77ae719ea1b8
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-14 22:07:29 +0000

    update single podcast

--- src/components/SinglePodcast.tsx
+++ src/components/SinglePodcast.tsx
@@ -5,7 +5,8 @@ interface data {
 	id: string;
 	title: string;
 	audio: string;
-	image: string;
+	big_cover_url: string;
+	author: string;
 }
 
 interface podcastProp {
@@ -16,20 +17,20 @@ function SinglePodcast({ data }: podcastProp) {
 	return (
 		<Link
 			href={`/explore/${data.id}`}
-			className="mx-auto h-[270px] w-full max-w-sm cursor-pointer rounded-3xl bg-[#1c1c1d] p-2 shadow-xl transition-colors hover:bg-[#1c1c1d4d]"
+			className="mx-auto  w-full max-w-xs cursor-pointer rounded-3xl bg-[#1c1c1d] p-2 shadow-xl transition-colors hover:bg-[#1c1c1d4d]"
 		>
 			<div className="h-[200px] rounded-xl">
 				<Image
 					className="h-full w-full rounded-3xl object-cover shadow-md"
 					alt=""
-					src={data.image}
+					src={data.big_cover_url}
 					width={100}
 					height={100}
 				/>
 			</div>
 			<div className="py-1 text-left leading-[1] text-white">
-				<h2 className="text-base font-medium">Kamasah Dickson</h2>
-				<span className="text-sm text-[#808080]">Kamasah Dickson</span>
+				<h2 className="my-2 text-base font-medium">{data.author}</h2>
+				<span className="text-sm text-[#808080]">{data.title}</span>
 			</div>
 		</Link>
 	);
