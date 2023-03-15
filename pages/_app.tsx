01820bd0ce971e8718d646da7565921edee89faf
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-15 18:44:14 +0000

    update application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Sidebar from "@/src/components/Sidebar";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 import { useRouter } from "next/router";
 import Link from "next/link";
@@ -34,35 +35,37 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<PodcastProvider>
-			<div className="my-max mx-auto grid w-full grid-cols-1 gap-1 pt-3 sm:flex sm:justify-between sm:gap-3 md:pt-0">
-				<div className="row-start-3 flex flex-1 md:flex-[2]">
-					<Sidebar
-						location={location}
-						setPlaying={setPlaying}
-						playing={playing}
-					/>
-				</div>
-				<main className="row-start-1 sm:flex-[4]">
-					<Link
-						href="/explore"
-						className=" relative block text-center md:hidden"
-					>
-						<h1 className="relative mx-auto w-fit py-3 text-4xl font-medium capitalize text-white">
-							<span className="text-purple-500">Po</span>dcast.
-							<div className="absolute -top-2 -right-3 text-[11px] text-gray-500">
-								{location}
-							</div>
-						</h1>
-					</Link>
-					<div>
-						<Component {...pageProps} />
+			<SkeletonTheme baseColor="#1c1c1d" highlightColor="#515151">
+				<div className="my-max mx-auto grid w-full grid-cols-1 gap-1 pt-3 sm:flex sm:justify-between sm:gap-3 md:pt-0">
+					<div className="row-start-3 flex flex-1 md:flex-[2]">
+						<Sidebar
+							location={location}
+							setPlaying={setPlaying}
+							playing={playing}
+						/>
 					</div>
-				</main>
+					<main className="row-start-1 sm:flex-[4]">
+						<Link
+							href="/explore"
+							className=" relative block text-center md:hidden"
+						>
+							<h1 className="relative mx-auto w-fit py-3 text-4xl font-medium capitalize text-white">
+								<span className="text-purple-500">Po</span>dcast.
+								<div className="absolute -top-2 -right-3 text-[11px] text-gray-500">
+									{location}
+								</div>
+							</h1>
+						</Link>
+						<div>
+							<Component {...pageProps} />
+						</div>
+					</main>
 
-				<div className="row-start-2 mx-auto w-full sm:flex-[2]">
-					<PlayingNow playing={playing} setPlaying={setPlaying} />
+					<div className="row-start-2 mx-auto w-full sm:flex-[2]">
+						<PlayingNow playing={playing} setPlaying={setPlaying} />
+					</div>
 				</div>
-			</div>
+			</SkeletonTheme>
 		</PodcastProvider>
 	);
 }
