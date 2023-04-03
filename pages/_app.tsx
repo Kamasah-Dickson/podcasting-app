561d150a4f6da6d8dfd87092b168698159550ffa
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-03 12:55:08 +0000

    update application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,16 +8,17 @@ import "react-loading-skeleton/dist/Skeleton.css";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { PodcastContext, PodcastProvider } from "@/src/context/podcastContext";
-import { IoIosArrowDown } from "react-icons/io";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const path = useRouter();
 	const { pathname } = path;
 	const [location, setLocation] = useState<string>("");
-	const { togglePlaying } = useContext(PodcastContext);
+	const [showPlaying, setShowPlaying] = useState(true);
+	const { playSinglePodcast } = useContext(PodcastContext);
 
 	useEffect(() => {
 		pathname == "/" && path.push("/explore");
+
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [pathname]);
 
@@ -38,14 +39,10 @@ export default function App({ Component, pageProps }: AppProps) {
 		<PodcastProvider>
 			<SkeletonTheme baseColor="#1c1c1d" highlightColor="#515151">
 				<div className="my-max mx-auto grid w-full grid-cols-1 gap-1 pt-3 sm:flex sm:justify-between sm:gap-3 md:pt-0">
-					<div className="row-start-3 flex flex-1 md:flex-[2]">
-						<Sidebar location={location} />
+					<div className="row-start-3 flex flex-1 md:flex-[1.5]">
+						<Sidebar setShowPlaying={setShowPlaying} location={location} />
 					</div>
-					<main
-						className={`row-start-1 shrink ${
-							!togglePlaying ? "sm:flex-[5]" : "sm:flex-[3.5]"
-						}`}
-					>
+					<main className={`row-start-1 shrink sm:flex-[3.5]`}>
 						<Link
 							href="/explore"
 							className=" relative block text-center md:hidden"
@@ -63,17 +60,15 @@ export default function App({ Component, pageProps }: AppProps) {
 					</main>
 
 					<div
-						className={`fixed ${
-							togglePlaying
-								? "hidden"
-								: "my-scroll top-0 left-0 row-start-2 mx-auto block  h-[90vh] w-full overflow-y-auto bg-[#0a0825d2] sm:fixed sm:h-full sm:flex-[2] sm:bg-transparent sm:py-0 md:flex-[3] lg:sticky xl:flex-[2]"
-						}`}
+						className={`fixed ${showPlaying && "lg:my-block hidden"}
+								 "my-scroll xl:flex-[2]" top-0 left-0 z-30 row-start-2 mx-auto block h-[90vh] w-full overflow-y-auto bg-[#0a0825de] sm:fixed sm:h-full sm:flex-[2] sm:bg-transparent sm:py-0 md:flex-[3] lg:sticky xl:flex-[2]
+						`}
 					>
-						<IoIosArrowDown
-							size={25}
-							className={`
-							mt-5 mb-3 w-full cursor-pointer text-white hover:text-purple-500 sm:hidden`}
-						/>
+						{playSinglePodcast.audioUrl && (
+							<p className="ml-16 pt-3 text-2xl font-bold text-white sm:hidden">
+								Playing Now...
+							</p>
+						)}
 						<PlayingNow />
 					</div>
 				</div>
