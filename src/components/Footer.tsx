c34d68c884c100aaf138443208cf9d845f1596cd
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-13 22:03:15 +0000

    update footer to v2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,15 +8,15 @@ function Footer() {
 	const { pathname } = useRouter();
 
 	return (
-		<div className=" my-scroll relative z-40 mt-3 bg-black py-3 px-5">
-			<div className="mb-16 flex flex-col items-center justify-between gap-3 md:mb-0 md:flex-row">
-				<ul className="hidden max-w-7xl gap-3 md:flex">
+		<div className=" my-scroll sticky -bottom-32 left-0 z-20 mt-3 w-full bg-black py-3 sm:fixed sm:bottom-0 lg:relative lg:px-5">
+			<div className="mb-20 flex flex-col justify-center gap-3 sm:mb-0 sm:flex-row ">
+				<ul className="mx-auto hidden flex-1 gap-1 lg:flex lg:max-w-full">
 					{sideLinks.map((data) => {
 						return (
 							<Link
 								className={` ${
-									pathname === data.path && "text-[#12a34f]"
-								} flex w-fit items-center gap-3 py-1 px-4 text-white hover:text-[#1eac59] active:scale-[1.2]`}
+									pathname === data.path && "text-[#1eac59]"
+								} flex w-fit items-center py-1 px-4 text-sm text-white hover:text-[#1eac59] active:scale-[1.2]`}
 								key={data.id}
 								href={data.path}
 							>
@@ -25,21 +25,26 @@ function Footer() {
 						);
 					})}
 				</ul>
-				<p className="mt-2 text-center text-base text-white">
+				<div className=" flex flex-1 items-center justify-center gap-7">
+					<Link href="">
+						<BsLinkedin color="white" size={15} />
+					</Link>
+					<Link href="">
+						<BsGithub color="white " size={15} />
+					</Link>
+					<Link href="">
+						<FaTwitter color="white" size={15} />
+					</Link>
+				</div>
+				<p className="flex-1 text-center text-sm text-white sm:flex-[2]">
 					Created by <span className="text-[#1e9c52]">Kamasah Dickson</span>{" "}
 					with 💚
 				</p>
-				<div className="flex items-center gap-7">
-					<Link href="">
-						<BsLinkedin color="white" size={20} />
-					</Link>
-					<Link href="">
-						<BsGithub color="white " size={20} />
-					</Link>
-					<Link href="">
-						<FaTwitter color="white" size={20} />
-					</Link>
-				</div>
+				<p className="flex-1 text-center text-sm text-white sm:flex-[2]">
+					&copy;
+					<span className="text-[#1e9c52]">{` ${new Date().getFullYear()} `}</span>{" "}
+					All rights reserved
+				</p>
 			</div>
 		</div>
 	);
