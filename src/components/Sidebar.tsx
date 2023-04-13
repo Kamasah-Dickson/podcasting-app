00d8aabe743b5705282344d641fea309cfb5102c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-13 22:03:00 +0000

    update sidebar to v2

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,7 +15,13 @@ function Sidebar({ location, setShowPlaying }: locationProp) {
 	const { togglePlaying, setTogglePlaying } = useContext(PodcastContext);
 
 	return (
-		<div className="fixed left-0 bottom-0 w-full bg-[#060807] sm:sticky sm:top-0 sm:left-0 sm:h-screen sm:bg-transparent">
+		<div
+			className={`fixed left-0 bottom-0 w-full border-t-4 ${
+				togglePlaying
+					? "border-green-500"
+					: "border-[#2d0796] md:border-[#0f9c4a]"
+			} bg-[#060807] sm:sticky sm:top-0 sm:left-0 sm:h-screen sm:border-none sm:bg-transparent`}
+		>
 			<Link href="/explore" className=" relative hidden text-center md:block">
 				<h1 className="relative mx-auto w-fit py-3 text-4xl font-medium capitalize text-white">
 					<span className="text-[#0f9c4a]">myPo</span>dcast.
@@ -25,13 +31,18 @@ function Sidebar({ location, setShowPlaying }: locationProp) {
 				</h1>
 			</Link>
 			<nav className="my-scroll mx-auto w-full overflow-auto xs:w-fit xs:overflow-hidden">
-				<ul className="flex w-full justify-center gap-4 p-3 py-3 text-gray-400 xs:w-fit sm:mt-5 sm:flex-col sm:py-0">
+				<ul className="flex w-full justify-center gap-4 px-3 py-3 text-gray-400 xs:w-fit sm:mt-5 sm:flex-col md:py-0">
 					{sideLinks.map((data) => {
 						return (
 							<Link
 								className={` ${
-									pathname === data.path && "my-hover"
-								} hover:my-hover flex w-fit items-center gap-3 py-2 px-4 active:scale-[1.2]`}
+									pathname === data.path &&
+									`${togglePlaying ? " my-hover" : "sm:my-hover my-hover2"}`
+								}   flex w-fit ${
+									togglePlaying
+										? "hover:my-hover"
+										: "sm:hover:my-hover hover:my-hover2"
+								} items-center gap-3 rounded-md py-2 px-4 active:scale-[1.2]`}
 								key={data.id}
 								href={`${data.path}`}
 							>
@@ -46,8 +57,10 @@ function Sidebar({ location, setShowPlaying }: locationProp) {
 							setTogglePlaying((prev) => !prev), setShowPlaying((prev) => !prev)
 						)}
 						className={` ${
-							togglePlaying ? null : "my-hover"
-						} hover:my-hover flex w-fit items-center gap-3 py-2 px-4 active:scale-[1.2] lg:hidden`}
+							togglePlaying
+								? "hover:my-hover"
+								: "my-hover2 sm:my-hover hover:my-hover2 sm:hover:my-hover"
+						}  flex w-fit items-center gap-3 rounded-md py-2 px-4 active:scale-[1.2] lg:hidden`}
 					>
 						<p className="text-xl">
 							<TbLayoutSidebarRight />
